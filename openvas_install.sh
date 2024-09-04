339cb12ce834725988bc80a47d20b0fdf713a05d
Ivan Reeve <ivanreevelopez@gmail.com>
2024-09-04 22:35:40 +0800

    Update openvas_install.sh

--- openvas_install.sh
+++ openvas_install.sh
@@ -4,6 +4,8 @@
 
 sudo apt install -y openvas
 sudo apt install -y gvm
-curl -LO https://github.com/greenbone/gsa/releases/download/v22.8.0/gsa-dist-22.8.0.tar.gz -o /tmp/v22.8.0
-sudo tar -xzf /tmp/v22.8.0/gsa-dist-22.8.0.tar.gz -C /usr/share/gvm/gsad/web/
+mkdir -p /tmp/openvas-autoinstall/
+curl -LO https://github.com/greenbone/gsa/releases/download/v22.8.0/gsa-dist-22.8.0.tar.gz -o /tmp/openvas-autoinstall/
+sudo tar -xzf /tmp/openvas-autoinstall/gsa-dist-22.8.0.tar.gz -C /usr/share/gvm/gsad/web/
+sudo rm -rf /tmp/openvas-autoinstall/
 echo "[LOG] Openvas successfully installed!"
